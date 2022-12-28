521d666940b9d7835605fab14549a22f528b3407
Michael Lavrisha <>
2022-12-28 07:50:27 -0700

    list lists

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -19,107 +22,106 @@ import java.util.Optional;
 
 @SpringBootApplication
 public class DemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@Bean
-	public ApplicationRunner runner(ChristmasListRepo repo) {
-		return (args -> {
-			ChristmasList list = new ChristmasList();
-			list.setName("FIRST!");
+    @Bean
+    public ApplicationRunner runner(ChristmasListRepo repo) {
+        return (args -> {
+            ChristmasList list = new ChristmasList();
+            list.setName("FIRST!");
 
-			repo.save(list);
-		} );
-	}
+            repo.save(list);
+        });
+    }
 
-	@Controller
-	static class ChristmasListController {
-		private final ChristmasListRepo repo;
+    @Controller
+    static class ChristmasListController {
+        private final ChristmasListRepo repo;
 
-		ChristmasListController(ChristmasListRepo repo) {
-			this.repo = repo;
-		}
+        ChristmasListController(ChristmasListRepo repo) {
+            this.repo = repo;
+        }
 
-		@GetMapping("/")
-		public ModelAndView index() {
-			ChristmasList list = new ChristmasList();
-			list.setId(1L);
-			ModelAndView mv = new ModelAndView("index");
-			mv.addObject("list", list);
-			return mv;
-		}
+        @GetMapping("/")
+        public ModelAndView index() {
+            ModelAndView mv = new ModelAndView("index");
+            mv.addObject("lists", repo.findAll());
+            return mv;
+        }
 
-		@PostMapping("/christmas-list/{listId}/items")
-		public String createItemInList(@PathVariable("listId") Long listId, Item item) {
-			ChristmasList list = repo.findById(listId).get();
-			ArrayList<Item> items = new ArrayList<>(list.getItems());
-			items.add(item);
-			list.setItems(items);
-			System.out.println(list);
-			repo.save(list);
+        @PostMapping("/christmas-list/{listId}/items")
+        public String createItemInList(@PathVariable("listId") Long listId, Item item) {
+            ChristmasList list = repo.findById(listId).get();
+            ArrayList<Item> items = new ArrayList<>(list.getItems());
+            items.add(item);
+            list.setItems(items);
+            repo.save(list);
 
-			return "redirect:/christmas-list/" + list.getId();
-		}
-		@DeleteMapping("/christmas-list/{listId}/items/{itemId}")
-		public ModelAndView removeListItem(@PathVariable("listId") Long listId, @PathVariable("itemId") Long itemId) {
-			ChristmasList list = repo.findById(listId).get();
-			list.getItems().removeIf(i -> i.getId().equals(itemId));
-			repo.save(list);
+            return "redirect:/christmas-list/" + list.getId();
+        }
 
-			ModelAndView mv = new ModelAndView("christmas-list-show");
-			mv.addObject("list", list);
-			return mv;
-		}
-		@PostMapping("/christmas-list")
-		public String createChristmasList(ChristmasList list) {
-			repo.save(list);
-			return "redirect:/christmas-list/" + list.getId();
-		}
+        @DeleteMapping("/christmas-list/{listId}/items/{itemId}")
+        public ModelAndView removeListItem(@PathVariable("listId") Long listId, @PathVariable("itemId") Long itemId) {
+            ChristmasList list = repo.findById(listId).get();
+            list.getItems().removeIf(i -> i.getId().equals(itemId));
+            repo.save(list);
 
-		@GetMapping("/christmas-list/{id}")
-		public String getChristmasList(@PathVariable("id") Long id, Model model) {
-			Optional<ChristmasList> byId = repo.findById(id);
+            ModelAndView mv = new ModelAndView("christmas-list-show");
+            mv.addObject("list", list);
+            return mv;
+        }
 
-			model.addAttribute("list", byId.get());
-			return "christmas-list-show";
-		}
-	}
+        @PostMapping("/christmas-list")
+        public String createChristmasList(ChristmasList list) {
+            repo.save(list);
+            return "redirect:/christmas-list/" + list.getId();
+        }
 
-	@Getter
-	@Setter
-	@ToString
-	@Entity
-	public static class ChristmasList {
-		@Id
-		@GeneratedValue()
-		Long id;
+        @GetMapping("/christmas-list/{id}")
+        public String getChristmasList(@PathVariable("id") Long id, Model model) {
+            Optional<ChristmasList> byId = repo.findById(id);
 
-		String name;
+            model.addAttribute("list", byId.get());
+            return "christmas-list-show";
+        }
+    }
 
-		@OneToMany(cascade = CascadeType.ALL)
-		List<Item> items;
+    @Getter
+    @Setter
+    @ToString
+    @Entity
+    public static class ChristmasList {
+        @Id
+        @GeneratedValue()
+        Long id;
 
-		public ChristmasList() {
-			id = null;
-			items = new ArrayList<>();
-			name = "";
-		}
-	}
+        String name;
 
-	@Getter
-	@Setter
-	@ToString
-	@Entity
-	public static class Item {
-		@Id
-		@GeneratedValue
-		Long id;
-		String description;
+        @OneToMany(cascade = CascadeType.ALL)
+        List<Item> items;
 
-		public Item() {
-			id = null;
-			description = "";
-		}
-	}
+        public ChristmasList() {
+            id = null;
+            items = new ArrayList<>();
+            name = "";
+        }
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    @Entity
+    public static class Item {
+        @Id
+        @GeneratedValue
+        Long id;
+        String description;
+
+        public Item() {
+            id = null;
+            description = "";
+        }
+    }
 }
