5b54f4cdbd0692114501f2bd90726891fccbafb7
Michael Lavrisha <>
2023-01-05 21:33:42 -0700

    Add server side validation to person creation form

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,26 @@
 package com.example.demo;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -52,8 +57,13 @@ public class DemoApplication {
         @GetMapping("/")
         public ModelAndView index() {
             ModelAndView mv = new ModelAndView("index");
-            mv.addObject("persons", repo.findAll());
+            setupPersonForm(mv, new PersonForm("", ""));
             return mv;
+        }
+
+        private void setupPersonForm(ModelAndView mv, PersonForm personForm) {
+            mv.addObject("persons", repo.findAll());
+            mv.addObject("personForm", personForm);
         }
 
         @PostMapping("/person/{personId}/items")
@@ -79,9 +89,21 @@ public class DemoApplication {
         }
 
         @PostMapping("/person")
-        public String createPerson(PersonEntity personEntity) {
+        public ModelAndView createPerson(@Valid PersonForm personForm, BindingResult bindingResult, HttpServletResponse response) {
+            if (bindingResult.hasErrors()) {
+                ModelAndView view = new ModelAndView("person-form");
+                view.setStatus(HttpStatusCode.valueOf(422));
+                setupPersonForm(view, personForm);
+                return view;
+            }
+            PersonEntity personEntity = new PersonEntity();
+            personEntity.setName(personForm.getName());
+            personEntity.setBuyingFor(personForm.getBuyingFor());
+
             repo.save(personEntity);
-            return "redirect:/person/" + personEntity.getId();
+            response.setHeader("HX-Location", "/person/" + personEntity.getId());
+            response.setStatus(204);
+            return null;
         }
 
         @GetMapping("/person/{id}")
@@ -90,6 +112,15 @@ public class DemoApplication {
 
             model.addAttribute("person", byId.get());
             return "person-show";
+        }
+
+        @Value
+        @AllArgsConstructor
+        private static class PersonForm {
+            @NotBlank
+            String name;
+            @NotBlank
+            String buyingFor;
         }
     }
 
@@ -106,7 +137,7 @@ public class DemoApplication {
 
         String buyingFor;
 
-        @OneToMany(cascade = CascadeType.ALL)
+        @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
         List<ItemEntity> items;
 
         public PersonEntity() {
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void validatePersonCreation() throws Exception {
+		mockMvc.perform(post("/person"))
+			.andExpect(status().is4xxClientError());
+	}
 }
